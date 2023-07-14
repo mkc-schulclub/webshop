f77caf2e4d82f835a20afb6f4c0f282c0f7a6200
okok7711 <62858707+okok7711@users.noreply.github.com>
2023-07-14 20:11:12 +0200

    remove reload... didnt work

--- backend/start.py
+++ backend/start.py
@@ -12,7 +12,6 @@ install()
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 1337))
     host = os.getenv("HOST", "0.0.0.0")
-    debug = bool_from_string(os.getenv("DEBUG")) # reload if debug
     if bool_from_string(os.getenv("USE_SSL", "false")):
         uvicorn.run(
             "app:app",
@@ -20,14 +19,10 @@ if __name__ == "__main__":
             host=host,
             ssl_keyfile=os.getenv("KEY_PATH"),
             ssl_certfile=os.getenv("CRT_PATH"),
-            reload=debug,
-            reload_dirs=["backend"] if debug else []
         )
     else:
         uvicorn.run(
             "app:app",
             port=port,
             host=host,
-            reload=debug,
-            reload_dirs=["backend"] if debug else []
         )
